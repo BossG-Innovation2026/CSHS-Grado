ad0d863b76c4081a6b908e9471c86ca4e914cbc4
serErick2026 <erickson.glodo@deped.gov.ph>
2026-09-06 21:31:14 +0800

    fix: remove stale DownloadStudentTemplate import from classes page

--- src/app/(protected)/classes/[id]/page.tsx
+++ src/app/(protected)/classes/[id]/page.tsx
@@ -12,7 +12,7 @@ import {
 import { RenameClassForm } from "../rename-class-form";
 import { AddClassSubjectForm } from "../add-class-subject-form";
 import { EnrollStudentForm } from "../enroll-student-form";
-import { UploadStudentsForm, DownloadStudentTemplate } from "../upload-students-form";
+import { UploadStudentsForm } from "../upload-students-form";
 import { SubjectTeacherSelect } from "../subject-teacher-select";
 import { RemoveClassSubjectButton } from "../remove-class-subject-button";
 import { UnenrollStudentButton } from "../unenroll-student-button";
@@ -177,7 +177,12 @@ export default async function ClassDetailPage({
               </p>
             </div>
             {canEdit && (
-              <DownloadStudentTemplate />
+              <a
+                href={`/classes/${cls.id}/student-template.xlsx`}
+                className="rounded-md bg-accent px-4 py-2 text-sm font-medium text-on-accent hover:bg-accent-strong"
+              >
+                Download template
+              </a>
             )}
           </div>
         </div>
